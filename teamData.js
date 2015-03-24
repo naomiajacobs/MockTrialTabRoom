354cf1d18ff4c513862c589e76c8725b6af8a572
naomiajacobs <naomiajacobs@gmail.com>
2015-03-24 13:38:07 -0700

    Update teamData.js

--- teamData.js
+++ teamData.js
@@ -3,18 +3,6 @@ var teams = [
   {"1091":
     {"school": "UChicago"},
     {"teamNum": "1091"},
-    {"roster":
-      {"member1": "naomiJacobs"},
-      {"member2": "reedMolbak"},
-      {"member3": "thomasChapman"},
-      {"member4": "alyssaMallory"},
-      {"member5": "maryiamSaifuddin"},
-      {"member6": "griffinBrunk"},
-      {"member7": "dexterOConnell"},
-      {"member8": "michaelHelger"},
-      {"member9": "meganKoehnen"},
-      {"member10": "emilyShaw"}
-    }
     {"runningBallots": 0},
     {"runningCS": 0},
     {"runningOCS": 0},
@@ -23,50 +11,8 @@ var teams = [
       {"round1":
         {"oppTeamNum": "1234"},
         {"side": "p"},
-        {"judge1":
-          {"score": 9},
-          {"awards":
-            {"atty":
-              {"naomiJacobs": 3},
-              {"alyssaMallory": 4},
-              {"thomasChapman": 5}
-            },
-            {"witness":
-              {"maryiamSaifuddin": 5},
-              {"griffinBrunk": 4},
-              {"reedMolbak": 3}
-            }
-          }
-        },
-        {"judge2":
-          {"score": 1},
-          {"awards":
-            {"atty":
-              {"naomiJacobs": 3},
-              {"reedMolbak": 4},
-              {"thomasChapman": 5}
-            },
-            {"witness":
-              {"maryiamSaifuddin": 5},
-              {"griffinBrunk": 4},
-              {"reedMolbak": 3}
-            }
-          }
-        }
-        {"judge3":
-          {"score": 9},
-          {"awards":
-            {"atty":
-              {"naomiJacobs": 3},
-              {"reedMolbak": 4},
-              {"thomasChapman": 5}
-            },
-            {"witness":
-              {"maryiamSaifuddin": 5},
-              {"griffinBrunk": 4}
-            }
-          }
-        }
+        {"judge1": 9},
+        {"judge2": 1}
       },
       {"round2": {}},
       {"round3": {}},
