1e5d994398c22685d87fdf5903145ae0fb274ffa
naomiajacobs <naomiajacobs@gmail.com>
2015-03-25 01:07:52 -0700

    Create interfaceJQueryCode.js

---
+++ interfaceJQueryCode.js
@@ -0,0 +1,15 @@
+//function that adds teams from same schools to each other's impermissibles list
+
+//function that adds opponents to each other's impermissibles list
+
+//function that updates a team's ballots
+
+//function that updates a team's CS
+
+//function that updates a team's OCS
+
+//function that updates a team's PD
+
+//function that adds judges' scores to teams
+
+//function that sets initial tournament info to general tournament data object (in data file) - invitational/regional/ORCS/nats, if regionals or ORCS, how many bids are available, coin flip
